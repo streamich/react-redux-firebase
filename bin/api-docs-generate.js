a811204d5c8f89a668f99e8ece9a46d933479695
prescottprue <sprue.dev@gmail.com>
2018-03-09 19:27:44 -0800

    update book

---
+++ bin/api-docs-generate.js
@@ -0,0 +1,67 @@
+const exec = require('child_process').exec
+const files = [
+  {
+    src: 'firebaseConnect.js',
+    dest: 'connect.md'
+  },
+  {
+    src: 'firestoreConnect.js',
+    dest: 'firestoreConnect.md'
+  },
+  {
+    src: 'withFirebase.js',
+    dest: 'withFirebase.md'
+  },
+  {
+    src: 'withFirestore.js',
+    dest: 'withFirestore.md'
+  },
+  {
+    src: 'createFirebaseInstance.js',
+    dest: 'firebaseInstance.md'
+  },
+  {
+    src: 'enhancer.js',
+    dest: 'enhancer.md'
+  },
+  {
+    src: 'helpers.js',
+    dest: 'helpers.md'
+  },
+  {
+    src: 'reducers.js',
+    dest: 'reducers.md'
+  },
+  {
+    src: 'reducer.js',
+    dest: 'reducer.md'
+  },
+  {
+    src: 'constants.js',
+    dest: 'constants.md'
+  }
+]
+const pathToDocumentationJs = 'node_modules/documentation/bin/documentation.js'
+
+const generateDocForFile = (file) => {
+  return new Promise((resolve, reject) => {
+    exec(`${pathToDocumentationJs} build src/${file.src} -f md -o docs/api/${file.dest} --shallow`, (error, stdout) => {
+      if (error !== null) {
+        return reject(error)
+      }
+      resolve(stdout)
+    })
+  })
+}
+
+(function () {
+  files.forEach(file => {
+    generateDocForFile(file)
+      .then((res) => {
+        console.log('Successfully generated', file) // eslint-disable-line no-console
+      })
+      .catch((err) => {
+        console.log('error generating doc: ', err) // eslint-disable-line no-console
+      })
+  })
+})()
